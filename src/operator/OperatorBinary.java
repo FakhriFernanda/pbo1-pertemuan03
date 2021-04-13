d2c79d39fef1b9679c89878f6d97e3e697131967
FakhriFernanda <fakhrifernanda@gmail.com>
2021-04-13 14:11:20 +0800

    Operator Binary Assignment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,13 @@
+package operator;
+
+public class OperatorBinary {
+
+    //operator (=) sebagai assignment/penugasan
+    //menempatkan nilai kedalam variabel
+    //baris 5-7 adalah coment/komentar yang tidak dijalankan oleh program
+
+    String nama = "Muhammad Fakhri Fernanda";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.89;
+
+}
